ff3618380a42ded4868ae20d05c7e7c99daa6f69
Youssef-Harby <youssef_harby@yahoo.com>
2022-01-14 21:46:09 +0200

    task 2

---
+++ JS-2/js-2.js
@@ -0,0 +1,30 @@
+
+function sumInput() {
+    let rule = true;
+    const inputNums = [];
+    // console.log(inputNums);
+    // console.log(typeof inputNums);
+    do {
+        numValue = prompt("Please enter numbers only");
+        // console.log(numValue);
+        // console.log(typeof numValue);
+        rule = /^[0-9]+$/.test(numValue);
+        // console.log(rule);
+        // console.log(typeof rule);
+
+        if (isNaN(numValue)) {
+            alert("Sorry, This is not a number")
+        } else {
+            inputNums.push(Number(numValue));
+        }
+    }
+    while (rule);
+
+    let sunNum = 0;
+    for (i in inputNums) {
+        sunNum += inputNums[i];
+        // console.log(startNum)
+    }
+    console.log(sunNum)
+    alert("The sum of the numbers you entered is " + sunNum);
+}
