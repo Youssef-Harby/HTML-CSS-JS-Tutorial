3877be3b77e861d74b60ce87af7278742e6fd534
Youssef Harby <youssef_harby@yahoo.com>
2022-02-03 15:59:54 +0200

    day5

---
+++ JS-4(EngMR)/f1.js
@@ -0,0 +1,5 @@
+console.log('Script1');
+
+for (i = 0; i <= 500; i++) {
+    console.log("Script1", i);
+}
---
+++ JS-4(EngMR)/f2.js
@@ -0,0 +1,5 @@
+console.log('Script2');
+
+for (i = 0; i <= 500; i++) {
+    console.log("script2", i);
+}
